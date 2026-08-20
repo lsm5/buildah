589e192fdb10174ae3a917da696d13b61f52da2e
Nalin Dahyabhai <nalin@redhat.com>
2026-08-20 14:14:42 -0400

    linter(staticcheck): don't set deprecated fields when setting up overlay
    Stop setting a pair of deprecated fields, spotted by golangci-lint v2.13.1.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>
    (cherry picked from commit b5ec7c93addee93addd090dfa5234188f253d97d)
    Signed-off-by: Lokesh Mandvekar <lsm5@redhat.com>

--- run_linux.go
+++ run_linux.go
@@ -1206,8 +1206,6 @@ func (b *Builder) runSetupVolumeMounts(mountLabel string, volumeMounts []string,
 			}
 
 			overlayOpts := overlay.Options{
-				RootUID:                idMaps.rootUID,
-				RootGID:                idMaps.rootGID,
 				UpperDirOptionFragment: upperDir,
 				WorkDirOptionFragment:  workDir,
 				GraphOpts:              slices.Clone(b.store.GraphOptions()),
